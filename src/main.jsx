e6b8489d2996886fcfa1b7dbfb30dcc5c84f288b
Samuel Garzotto <fsantos.samuca@gmail.com>
2023-01-16 09:48:36 -0300

    att-hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Routes } from './routes';
+import { AuthProvider } from './hooks/auth'
 
 import theme from './styles/theme';
 import { ThemeProvider } from 'styled-components';
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <AuthProvider>
         <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import { Container, Form, Background } from './styles';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 
+import { useAuth } from '../../hooks/auth';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { ButtonText } from '../../components/ButtonText';
 
 export function SignIn() {
+    const [ email, setEmail ] = useState('')
+    const [ password, setPassword ] = useState('')
     const navigate = useNavigate()
+
+    const { singIn } = useAuth()
+
+    function handleSingIn() {
+        singIn({ email, password })
+    }
 
     return (
         <Container>
@@ -17,10 +27,10 @@ export function SignIn() {
 
                 <h2>Faça seu login</h2>
 
-                <Input icon={FiMail} placeholder='E-mail' />
-                <Input icon={FiLock} placeholder='Senha' />
+                <Input type='email' icon={FiMail} placeholder='E-mail' onChange={e => setEmail(e.target.value)} />
+                <Input type='password' icon={FiLock} placeholder='Senha' onChange={e => setPassword(e.target.value)} />
 
-                <Button className='login-button' title='Entrar' />
+                <Button onClick={handleSingIn} className='login-button' title='Entrar' />
                 <ButtonText onClick={() => navigate('/register')} className='text-button' title='Criar conta' />
 
             </Form>
